23a268a3b5b6d37a627a897b11821d8613d93034
Vital-stude <110226252+Vital-stude@users.noreply.github.com>
2022-08-10 18:56:07 +0300

    два ответа на Урок Условных Операторов

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
 
+        int clientOs = 1;
+        if (clientOs == 1) {
 
-    {
-        int clientOS = (0 || 1);
-        int clientIOS = 0;
-        int clientAndroid = 1;
-
-
-        if (clientOS = 0) ;
-        {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
-
-        if (clientOS = 1) ;
-        {
-            System.out.println("Установите версию приложения для Андроид по ссылке");
-        }
-    }
-
-    public static void task2(String[] args) {
-        System.out.println("ЗАДАНИЕ 2");
-    }
-
-    {
-        int clientDeviceYear = a;
-        int clientOS = (0 || 1);
-        int clientIOS = 0;
-        int clientAndroid = 1;
-
-        if (0a < 2015);
-        {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        if (1a < 2015);
-        {
-            System.out.println("Установите облегченную версию приложения для Андроид по ссылке");
-        }
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");}
 
 
-        if (0a >= 2015);
-        {
-            System.out.println("Установите приложение для iOS по ссылке");
-        }
-        if (1a >= 2015);
-        {
-            System.out.println("Установите приложение для Андроид по ссылке");
-        }
-    }
-}
+}}
