57fe03bf0116a58c1c4347fb663ed93253183683
Vital-stude <110226252+Vital-stude@users.noreply.github.com>
2022-08-10 19:50:43 +0300

    два ответа на Урок Условных Операторов

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
 
         int clientOs = 1;
         if (clientOs == 1) {
@@ -8,7 +8,23 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         if (clientOs == 0) {
-            System.out.println("Установите версию приложения для IOS по ссылке");}
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        }
 
 
-}}
+    }
+
+    public static void jobe() {
+        System.out.println("Задание 2");}
+
+    {
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientDeviceYear >= 2015) {
+            System.out.println("Установите облегченную версию приложения для Андроид по ссылке");
+        }
+    }
+
+}
